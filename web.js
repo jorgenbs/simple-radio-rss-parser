2d71340b7911fdd2f420b4ee6c94e5a7859cb091
jorgenbs <jorgenbs@gmail.com>
2013-08-23 12:07:17 +0200

    dunno

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var http = require('http');
-var parser = require('./parser');
 var path = require('path')
+var routes = require('./routes')
 
 var app = module.exports = express();
 app.use(express.logger());
@@ -13,22 +13,13 @@ app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-//app.use('/', express.static(path.join(__dirname, 'views')));
 app.use('/lib', express.static(path.join(__dirname, 'public/js/lib')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 //routes
-// redirect all others to the index (HTML5 history)
-app.get('/rss', function(req, res) {
-	parser.getXMLString(function (xmlString) {
-		var list = parser.parseXMLString(xmlString);
-		res.send(list);
-	})
-});
-app.get('/', function(req, res) {
-	res.render('index');
-});
+app.get('/', routes.index);
+app.get('/rss', routes.rss);
 
 //init webserver
 var PORT = process.env.PORT || 5000;
