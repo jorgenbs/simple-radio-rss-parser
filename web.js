68d1bc9cf8ffa2548a55534de5d76467f5b35021
jorgenbs <jorgenbs@gmail.com>
2013-08-23 11:56:02 +0200

    using jade template

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 var http = require('http');
 var parser = require('./parser');
 var path = require('path')
-//var routes = require('./routes')
 
 var app = module.exports = express();
 app.use(express.logger());
@@ -10,11 +9,11 @@ app.use(express.logger());
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-//app.set('view engine', 'jade');
+app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use('/', express.static(path.join(__dirname, 'views')));
+//app.use('/', express.static(path.join(__dirname, 'views')));
 app.use('/lib', express.static(path.join(__dirname, 'public/js/lib')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
@@ -26,6 +25,9 @@ app.get('/rss', function(req, res) {
 		var list = parser.parseXMLString(xmlString);
 		res.send(list);
 	})
+});
+app.get('/', function(req, res) {
+	res.render('index');
 });
 
 //init webserver
